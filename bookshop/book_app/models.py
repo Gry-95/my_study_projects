619a6c252091115c9d19b74665efbae3f1fb2622
Andrey Grinenko <green37tea@gmail.com>
2023-08-12 11:06:40 +0300

    creating htmls in book_app and config them

--- bookshop/book_app/models.py
+++ bookshop/book_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-
+from django.urls import reverse
+from django.utils.text import slugify
 
 # Create your models here.
 
@@ -8,7 +9,14 @@ class Book(models.Model):
     rating = models.IntegerField()
     is_best_selling = models.BooleanField()
     author = models.CharField(null=True, max_length=100)
+    slug = models.SlugField(default='', null=False, db_index=True)
 
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title, allow_unicode=True)
+        super(Book, self).save(*args, **kwargs)
+
+    def get_url(self):
+        return reverse("book-detail", args=[self.slug])
 
     def __str__(self):
         return f'{self.title} {self.rating}'
--- bookshop/book_app/views.py
+++ bookshop/book_app/views.py
@@ -1,5 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Book
 
 # Create your views here.
-def some_func():
-    pass
+def show_all_books(request):
+    books = Book.objects.all()
+    return render(request, 'book_app/all_books.html', {
+        "books": books
+    })
+
+
+def show_one_book(request, slug_book:str):
+    book = get_object_or_404(Book, slug=slug_book)
+    return render(request, 'book_app/one_book.html', {
+        "book": book
+    })
