e0892f3c959cace6ee9a3f4626dfd312489c8b4c
Andrey Grinenko <green37tea@gmail.com>
2023-08-13 20:45:49 +0300

    admin panel configuration

--- bookshop/book_app/admin.py
+++ bookshop/book_app/admin.py
@@ -5,16 +5,47 @@ from .models import Book
 
 
 # Register your models here.
+class Rating_filter(admin.SimpleListFilter):
+    title = 'Фильтр по рейтингу'
+    parameter_name = 'rating'
+
+    def lookups(self, request, model_admin):
+        return [
+            ('<40', 'Рейтинг низкий'),
+            ('от 40 до 59', 'Рейтинг средний'),
+            ('от 60 до 79', 'Рейтинг высокий'),
+            ('>=80', 'Рейтинг максимальный')
+        ]
+
+    def queryset(self, request, queryset: QuerySet):  # Добавляем в фильтр разделение по рейтингу
+        if self.value() == '<40':
+            return queryset.filter(rating__lt=40)
+        if self.value() == 'от 40 до 59':
+            return queryset.filter(rating__gt=39, rating__lt=60)
+        if self.value() == 'от 60 до 79':
+            return queryset.filter(rating__gt=59, rating__lt=80)
+        return queryset.filter(rating__gt=79)
+
+
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    list_display = ['title', 'rating', 'is_best_selling', 'currency', 'author', 'slug', 'rating_status'] #Добавление полей в админку
-    list_editable = ['rating', 'currency','is_best_selling', 'author'] #редактирование поля
-    list_per_page = 15
-    ordering = ['-rating']
-    actions = ['set_dollars', 'set_euro']
-    search_fields = ['title']
+    # fields = ['title', 'rating'] #В добавление элемента в ДБ указываем поля для заполнения
+    exclude = ['slug']  # Выбираем поля которые исключим из добавления в БД
+    list_display = ['title', 'rating', 'is_best_selling', 'currency', 'author', 'slug',
+                    'rating_status']  # Добавление полей в админку
+    prepopulated_fields = {
+        'slug': (
+            'name',
+        )}  # Указываем что поле slug предвычисляемое и оно будет заполняться автоматичски на основе введенного имени
+    readonly_fields = ['author']  # Выбираем поля которые пользватель не сможет редактивровать
+    list_editable = ['rating', 'currency', 'is_best_selling', 'author']  # редактирование поля
+    list_per_page = 15  # Указываем сколько записей помещается на странице
+    ordering = ['-rating']  # Сортировка по рейтингу
+    actions = ['set_dollars', 'set_euro']  # Добавили возможность изменять валюту выбранным элементам БД
+    search_fields = ['title']  # Добавляем в поиск поля для поиска (по которым будем искать)
+    list_filter = ['title', Rating_filter]  # Добавляем в фильтр данные
 
-    @admin.display(ordering='rating', description='Статус') #Добавляет колонку статус
+    @admin.display(ordering='rating', description='Статус')  # Добавляет колонку статус
     def rating_status(self, book: Book):
         if book.rating < 70:
             return 'Не стоит внимания'
@@ -23,7 +54,7 @@ class BookAdmin(admin.ModelAdmin):
         return 'Стоит прочитать'
 
     @admin.action(description='Валюту в доллар')
-    def set_dollars(self, request, qs: QuerySet): #Проставляет в колонке currency Dollars
+    def set_dollars(self, request, qs: QuerySet):  # Проставляет в колонке currency Dollars
         qs.update(currency=Book.USD)
 
     @admin.action(description='Валюту в Евро')
@@ -33,6 +64,4 @@ class BookAdmin(admin.ModelAdmin):
             request,
             f'Было обновлено {count_updated} значений')
 
-
-
-#admin.site.register(Book, BookAdmin) - Вместо декоратора
+# admin.site.register(Book, BookAdmin) - Вместо декоратора
--- bookshop/book_app/models.py
+++ bookshop/book_app/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.urls import reverse
 from django.utils.text import slugify
+from django.core.validators import MaxValueValidator, MinValueValidator
+
 
 # Create your models here.
 
@@ -14,7 +16,9 @@ class Book(models.Model):
         (RUB, 'RUBLIES'),
     ]
     title = models.CharField(max_length=70)
-    rating = models.IntegerField()
+    rating = models.IntegerField(validators=[MinValueValidator(1),
+                                             MaxValueValidator(100)]
+                                 )  # Устанавливаем минимальное и маскимальное значение добавления записи в БД
     is_best_selling = models.BooleanField()
     author = models.CharField(null=True, max_length=100, blank=True)
     currency = models.CharField(max_length=3, choices=CURRENCY_CHOICES, default=RUB)
