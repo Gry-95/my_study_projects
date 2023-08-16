6ac3ad04231c4cbe8662fc1c1da864c3a01b337c
Andrey Grinenko <green37tea@gmail.com>
2023-08-16 14:40:20 +0300

    new model and db

--- movie_project/movie_app/admin.py
+++ movie_project/movie_app/admin.py
@@ -1,16 +1,18 @@
 from django.contrib import admin
 from django.db.models import QuerySet
 from django.contrib.auth.models import User
-from .models import Movie, Director, Actor
+from .models import Movie, Director, Actor, DressingRoom
 
 
 # Register your models here.
+
+
 @admin.register(Movie)
 class MovieAdmin(admin.ModelAdmin):
     list_display = ['id', 'name', 'rating', 'year', 'budget', 'director']
     list_editable = ['name', 'rating', 'year']
     prepopulated_fields = {'slug': ('name',)}
-    filter_horizontal = ['actors']  # Добавляет окошко для удобства добавление актеров к фильму
+    # filter_horizontal = ['actors']  # Добавляет окошко для удобства добавление актеров к фильму
     ordering = ['-rating']  # Сортировка по рейтинку
 
 
@@ -24,3 +26,9 @@ class DirectorAdmin(admin.ModelAdmin):
 class ActorAdmin(admin.ModelAdmin):
     list_display = ['id', 'first_name', 'last_name', 'gender']
     list_editable = ['first_name', 'last_name', 'gender']
+
+
+@admin.register(DressingRoom)
+class DressingRoomAdmin(admin.ModelAdmin):
+    list_display = ['id', 'floor', 'number']
+    list_editable = ['floor', 'number']
--- movie_project/movie_app/models.py
+++ movie_project/movie_app/models.py
@@ -4,29 +4,6 @@ from django.utils.text import slugify
 
 
 # Create your models here.
-class Actor(models.Model):
-    MALE = 'M'
-    FEMALE = 'F'
-    GENDERS = [
-        (MALE, 'Мужчина'),
-        (FEMALE, 'Женщина'),
-    ]
-
-    first_name = models.CharField(max_length=70)
-    last_name = models.CharField(max_length=70)
-    gender = models.CharField(max_length=1, choices=GENDERS, default=MALE)
-    objects = models.Manager()
-
-    def __str__(self):
-        if self.gender == self.MALE:
-            return f'Актер - {self.first_name} {self.last_name}'
-        else:
-            return f'Актриса - {self.first_name} {self.last_name}'
-
-    def get_url_act(self):
-        return reverse('actor-detail', args=[self.id])
-
-
 class Director(models.Model):
     first_name = models.CharField(max_length=70)
     last_name = models.CharField(max_length=70)
@@ -40,13 +17,43 @@ class Director(models.Model):
         return reverse('director-detail', args=[self.id])
 
 
+class DressingRoom(models.Model):
+    floor = models.IntegerField(default=1)
+    number = models.IntegerField(default=2)
+
+    def __str__(self):
+        return f'{self.floor} {self.number}'
+
+
+class Actor(models.Model):
+    MALE = 'M'
+    FEMALE = 'F'
+    GENDERS = [
+        (MALE, 'Мужчина'),
+        (FEMALE, 'Женщина'),
+    ]
+
+    first_name = models.CharField(max_length=70)
+    last_name = models.CharField(max_length=70)
+    dressing = models.OneToOneField(DressingRoom, on_delete=models.SET_NULL, null=True, blank=True)
+    gender = models.CharField(max_length=1, choices=GENDERS, default=MALE)
+
+    def __str__(self):
+        if self.gender == self.MALE:
+            return f'Актер - {self.first_name} {self.last_name}'
+        else:
+            return f'Актриса - {self.first_name} {self.last_name}'
+
+    def get_url_act(self):
+        return reverse('actor-detail', args=[self.id])
+
+
 class Movie(models.Model):
     name = models.CharField(max_length=40)
     rating = models.IntegerField()
     year = models.IntegerField(null=True)
     budget = models.IntegerField(default=1000000)
     slug = models.SlugField(default='', null=False, db_index=True)  # дб индекс для быстрого поиска при расширении бд
-    objects = models.Manager()
     director = models.ForeignKey(Director, on_delete=models.CASCADE, null=True)
     actors = models.ManyToManyField(Actor)
 
