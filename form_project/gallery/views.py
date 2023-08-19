913c9c48483ed0f511180def709fcfe9e2dd5d86
Andrey Grinenko <green37tea@gmail.com>
2023-08-19 11:38:55 +0300

    feedback done

--- form_project/gallery/views.py
+++ form_project/gallery/views.py
@@ -1,18 +1,43 @@
 from django.shortcuts import render
 from django.views import View
+from django.http import HttpResponseRedirect
+from django.views.generic.edit import CreateView
+from django.views.generic import ListView
+from .forms import GalleryUploadForm
+from .models import Gallery
 
 
 # Create your views here.
-def storage_file(file):
-    with open(f'gallery_tmp/{file.name}', 'wb+') as new_file:
-        for chunk in file.chunks():
-            new_file.write(chunk)
+
+class ListGallery(ListView):
+    model = Gallery
+    template_name = 'gallery/list_file.html'
+    context_object_name = 'records'
 
 
-class GalleryView(View):
-    def get(self, request):
-        return render(request, 'gallery/load_file.html')
+# def storage_file(file): #Если нет модели юзаем это, иначе galleryview post заменяет эту функцию
+#   with open(f'gallery_tmp/{file.name}', 'wb+') as new_file:
+#      for chunk in file.chunks():
+#         new_file.write(chunk)
 
-    def post(self, request):
-        storage_file(request.FILES['image'])
-        return render(request, 'gallery/load_file.html')
+
+# class GalleryView(View):
+#     def get(self, request):
+#         form = GalleryUploadForm()
+#         return render(request, 'gallery/load_file.html', {'form': form})
+#
+#     def post(self, request):
+#         form = GalleryUploadForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             new_image = Gallery(image=form.cleaned_data['image'])
+#             new_image.save()
+#             return HttpResponseRedirect('load_image')
+#         return render(request, 'gallery/load_file.html', {'form': form})
+
+
+class CreateGalleryView(CreateView):
+    model = Gallery
+    form_class = GalleryUploadForm
+    # fields = '__all__'
+    template_name = 'gallery/load_file.html'
+    success_url = 'load_image'
